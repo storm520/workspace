0faddfa76819ee67efc86bca2804d6fc48813e7c
wf <1165289428@qq.com>
2017-05-30 17:29:27 +0800

    third

---
+++ taotao-common/src/main/java/com/taotao/common/EUTreeNode.java
@@ -0,0 +1,26 @@
+package com.taotao.common;
+//返回的json数据
+public class EUTreeNode {
+private long id;
+private String text;
+private String staute;
+public long getId() {
+	return id;
+}
+public void setId(long id) {
+	this.id = id;
+}
+public String getText() {
+	return text;
+}
+public void setText(String text) {
+	this.text = text;
+}
+public String getStaute() {
+	return staute;
+}
+public void setStaute(String staute) {
+	this.staute = staute;
+}
+
+}
